e83fc10832e779a9c469a39144d644b873e37027
Gonzalo Daniel Aguilar <42863568+gonzalolater@users.noreply.github.com>
2022-12-03 16:55:21 -0300

    minor

--- contracts/Bull&Bear.sol
+++ contracts/Bull&Bear.sol
@@ -1,7 +1,8 @@
-// SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.0;
 
+// Open Zeppelin Imports
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
